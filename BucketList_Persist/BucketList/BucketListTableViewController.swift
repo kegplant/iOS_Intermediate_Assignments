645c02d918b28d09370de3ae4f7377aaca1b6b44
kegplant <ace1905@gmail.com>
2018-01-18 10:56:32 -0800

    bucketlist now persists

--- BucketList_Persist/BucketList/BucketListTableViewController.swift
+++ BucketList_Persist/BucketList/BucketListTableViewController.swift
@@ -7,21 +7,36 @@
 //
 
 import UIKit
+import CoreData
 
 class BucketListTableViewController: UITableViewController, AddMissionTableViewControllerDelegate {
-
-    var missions=["Earn 2 Mils","Travel to Mars"]
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var missions:[BucketList]=[]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //do context again? or no need
+        missions=fetch()
     }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return missions.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell=tableView.dequeueReusableCell(withIdentifier: "MissionCell", for: indexPath)
-        cell.textLabel!.text=missions[indexPath.row]
+        cell.textLabel!.text=missions[indexPath.row].mission
         return cell
+    }
+    func fetch()->[BucketList]{
+        let bucketlistRequest:NSFetchRequest<BucketList> = BucketList.fetchRequest()
+        do {
+            let fetchedMissions = try context.fetch(bucketlistRequest)
+            return fetchedMissions
+        }
+        catch {
+            print("fetch error: ")
+            print(error)
+            return []
+        }
     }
     override func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) {
         performSegue(withIdentifier: "EditMissionSegue", sender: indexPath)
@@ -36,7 +51,7 @@ class BucketListTableViewController: UITableViewController, AddMissionTableViewC
             let controller = navigationController.topViewController as! AddMissionTableViewController
             controller.delegate = self
             if let indexPath = sender as? IndexPath {
-                controller.stringFromSegue = missions[indexPath.row]
+                controller.stringFromSegue = missions[indexPath.row].mission
                 controller.indexPathFromSegue = indexPath
             }
 //            else if let cell = sender as? UITableViewCell{//which function is calling this segue???
@@ -51,10 +66,13 @@ class BucketListTableViewController: UITableViewController, AddMissionTableViewC
     }
     func addMissionViewController(controller: AddMissionTableViewController, didFinishAddingItem item: String, at indexPath:IndexPath?) {
         if let index = indexPath?.row {
-            missions[index]=item
+            missions[index].mission=item
         }else {
-            missions.append(item)
+            let newMission=BucketList(context:context)
+            newMission.mission=item
+            missions.append(newMission)
         }
+        missionSave()
         dismiss(animated: true, completion: nil)
         tableView.reloadData()
     }
@@ -63,8 +81,19 @@ class BucketListTableViewController: UITableViewController, AddMissionTableViewC
         dismiss(animated: true,completion:nil)
     }
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        context.delete(missions[indexPath.row])
+        missionSave()
         missions.remove(at: indexPath.row)
         tableView.reloadData()
+        
+    }
+    func missionSave(){
+        do{
+            try context.save()
+        } catch{
+            print("core data save error: ")
+            print(error)
+        }
     }
 }
 
